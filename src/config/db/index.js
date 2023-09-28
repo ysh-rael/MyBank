14bc68f6014a28595346feeef54df979409db148
Yshrael <suporte2@brasolucoes.com>
2023-09-28 11:37:04 -0300

    code documentation

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,12 +1,16 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
-
-const user = process.env.DB_USER;
-const pass = process.env.DB_PASS;
-const bank = process.env.DB_BANK;
-
 module.exports = class {
-    static async connect () {
+/**
+     * Esta é uma função estática com parâmetro de autênticação para conexão com o banco.
+     * Por padrão utiliza variáveis de ambiente para autênciação
+     *
+     * @param {string} user - autenticação: usuário. (default: DB_USER)
+     * @param {string} pass - autenticação: senha. (default: DB_PASS)
+     * @param {string} bank - banco a ser utilizado. (default: DB_BANK)
+     * @returns {Promise<typeof mongoose>} - A soma dos dois números.
+ */
+    static async connect(user = process.env.DB_USER, pass = process.env.DB_PASS, bank = process.env.DB_BANK) {
         return await mongoose.connect(`mongodb+srv://${user}:${pass}@${bank}.mongodb.net/`);
     }
 };
